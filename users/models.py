bc5ec24a4a19f56f3a5692223b3ee41767c2012f
suraj_sagar <32412000+surajsgr@users.noreply.github.com>
2019-05-27 21:30:11 +0530

    final commit

--- users/models.py
+++ users/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+from django.utils import timezone
+
+
+# class Group(models.Model):
+#     Group_name = models.CharField(max_length=100,blank=True,default=None)
+#     created_date = models.DateTimeField(default=timezone.now)
+
 
 
 class Profile(models.Model):
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    # group_name=models.ForeignKey(Group,on_delete=models.CASCADE)
+
 
     def __str__(self):
         return f'{self.user.username} Profile'
@@ -19,3 +29,5 @@ class Profile(models.Model):
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
+
+
